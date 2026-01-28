8e13942d44ff4e6bd747cff2b730e50b581de71c
FelipeGA02 <felipe.gurgel5@gmail.com>
2026-01-27 21:11:08 -0300

    feat: DAU and MAU

--- src/analytics/dau.sql
+++ src/analytics/dau.sql
@@ -1,6 +1,9 @@
-SELECT substr(DtCriacao,0, 11) AS DtDia,
-       count(DISTINCT IdCliente) AS DAU
-
+/* 
+  Daily Active Users (DAU) Report
+  This query calculates the number of unique active users per day.
+*/
+SELECT substr(DtCriacao, 0, 11) AS DtDia, 
+        count(DISTINCT IdCliente) AS DAU
 FROM transacoes
 GROUP BY 1
-ORDER BY DtDia
+ORDER BY DtDia;
--- src/analytics/mau.sql
+++ src/analytics/mau.sql
@@ -1,27 +1,31 @@
+/* 
+  Monthly Active Users (MAU) Report
+  This query calculates the number of unique active users per month.
+*/
+
 WITH tb_daily AS (
 
-    SELECT DISTINCT 
-        date(substr(DtCriacao,0,11)) AS DtDia,
+    SELECT DISTINCT
+        date(substr(DtCriacao, 1, 11)) AS DtDia,
         IdCliente
 
     FROM transacoes
-    order by DtDia
+    ORDER BY DtDia
 
 ),
 
-tb_distinct_day AS (
+tb_distinct_days AS (
 
     SELECT
-            DISTINCT DtDia AS dtRef
+        DISTINCT DtDia AS dtRef
     FROM tb_daily
 
 )
 
 SELECT t1.dtRef,
-       count( distinct IdCliente) AS MAU,
-       count(distinct t2.dtDia) AS qtdeDias
-
-FROM tb_distinct_day AS t1
+        COUNT(DISTINCT IdCliente) AS MAU,
+        COUNT(DISTINCT t2.DtDia) AS qtdeDias
+FROM tb_distinct_days AS t1
 
 LEFT JOIN tb_daily AS t2
 ON t2.DtDia <= t1.dtRef
@@ -29,4 +33,4 @@ AND julianday(t1.dtRef) - julianday(t2.DtDia) < 28
 
 GROUP BY t1.dtRef
 
-ORDER BY t1.dtRef ASC
+ORDER BY t1.dtRef asc
